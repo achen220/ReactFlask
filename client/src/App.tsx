179ab9798fedec3ce4d604ddf2e99d85018f5042
achen9149 <aaronchen9149@gmail.com>
2023-11-18 19:40:24 -0500

    able to download excel file from frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,37 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [playlist, setPlaylist] = useState<string>('');
-  const sendLink = async (e, link:string) => {
+  const sendLink = async (e) => {
     e.preventDefault();
     try {
       const res = await fetch('/api', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body:JSON.stringify({playlistLink: link})
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
       })
+      const resBlob = await res.blob();
+      // Create a link element
+      const link = document.createElement('a');
+
+      // Create a Blob URL from the response blob
+      const blobUrl = URL.createObjectURL(resBlob);
+
+      // Set the link's href to the Blob URL
+      link.href = blobUrl;
+
+      // Set the link's download attribute with the desired file name
+      link.download = 'example.xlsx';
+
+      // Append the link to the document
+      document.body.appendChild(link);
+
+      // Trigger a click on the link to initiate the download
+      link.click();
+
+      // Remove the link from the document
+      document.body.removeChild(link);
+
+      // Revoke the Blob URL to free up resources
+      URL.revokeObjectURL(blobUrl);
       if (!res.ok) {
         throw new Error('HTTP Error: ' + res.status);
       }
@@ -21,8 +43,7 @@ function App() {
     }
   }
   return (
-    <form onSubmit={(e) => sendLink(e,playlist)}>
-      <input type="text" placeholder="spotify link" onChange={(e) => {setPlaylist(e.target.value)}}/>
+    <form onSubmit={(e) => sendLink(e)}>
       <input type="submit" />
     </form>
   )
--- server/server.py
+++ server/server.py
@@ -1,43 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 import json
-
+from openpyxl import Workbook
+from io import BytesIO
 #__name__ determines the location of this file
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/api",methods=["GET","POST"])
-def home():
-  if request.method == 'POST':
-    #get the string format of the page and parse it into html
-    URL = "https://books.toscrape.com/catalogue/page-1.html"
+def webscrape():
+  #get the string format of the page and parse it into html
+  URL = "https://books.toscrape.com/catalogue/page-1.html"
+  result = requests.get(URL)
+  doc = BeautifulSoup(result.text, "html.parser")
+  #find the maxpage so we can interate through them
+  maxPage = int(doc.find(class_="current").text.strip().split(' ')[-1])
+  #used to store all the infos
+  infoStorage = []
+  #iterate from first page to the maxpage storing all info into infoStorage
+  for i in range(1,maxPage + 1):
+    URL = f"https://books.toscrape.com/catalogue/page-{i}.html"
     result = requests.get(URL)
     doc = BeautifulSoup(result.text, "html.parser")
-    #find the maxpage so we can interate through them
-    maxPage = int(doc.find(class_="current").text.strip().split(' ')[-1])
-    #used to store all the infos
-    infoStorage = []
-    #iterate from first page to the maxpage storing all info into infoStorage
-    for i in range(1,maxPage + 1):
-      URL = f"https://books.toscrape.com/catalogue/page-{i}.html"
-      result = requests.get(URL)
-      doc = BeautifulSoup(result.text, "html.parser")
-      booksInfo = doc.find_all(class_="product_pod")
-      #iterating from all books in a page
-      for book in booksInfo:
-        info = {}
-        title = book.find('h3').contents[0].get('title')
-        info['title'] = title
-        price = book.find(class_="price_color").text[2:len(book.find(class_="price_color").text)]
-        info['price'] = price
-        availability = book.find(class_="instock availability").text.strip()
-        info['availability'] = availability
-        infoStorage.append(info)
-      with open('output.html', 'w',encoding = 'utf-8') as file:
-        file.write(str(infoStorage))
-  return render_template("index.html")
+    booksInfo = doc.find_all(class_="product_pod")
+    #iterating from all books in a page
+    for book in booksInfo:
+      info = {}
+      title = book.find('h3').contents[0].get('title')
+      info['title'] = title
+      price = book.find(class_="price_color").text[2:len(book.find(class_="price_color").text)]
+      info['price'] = price
+      availability = book.find(class_="instock availability").text.strip()
+      info['availability'] = availability
+      infoStorage.append(info)
+  #write to an excel file
+  workbook = Workbook()
+  sheet = workbook.active
+  # Write data to the Excel sheet
+  sheet['A1'] = 'Name'
+  sheet['B1'] = 'Age'
+  sheet['A2'] = 'John'
+  sheet['B2'] = 25
+  sheet['A3'] = 'Alice'
+  sheet['B3'] = 30
+
+  # Save the workbook to a BytesIO object
+  excel_data = BytesIO()
+  workbook.save(excel_data)
+  excel_data.seek(0)
+
+  # Return the Excel file as a response
+  return send_file(
+      excel_data,
+      mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      as_attachment=True,
+      download_name='example.xlsx'
+  )
+
 
 #only run server if this script is being ran in the file its created rather than imported
 if __name__ == "__main__":
